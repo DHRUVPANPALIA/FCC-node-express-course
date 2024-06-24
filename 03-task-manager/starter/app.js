02b62ac9c5e6b40651601d88484c2ab5a2ab129e
Dhruv Panpalia <21dit043@charusat.edu.in>
2024-06-24 13:53:12 +0530

    Progress

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const task = require('./routes/tasks');
+
+// middleware
+app.use(express.json())
+
 
 // routes examples
 app.get('/hello', (req, res) => {
     res.send('Task Manager App')
 })
+
+
+
+app.use('/api/v1/tasks', task)
+
+
 
 const port = 3000
 
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,26 @@
+const getAllTasks = (req, res) => {
+    res.send('All items')
+}
+
+const createTask = (req, res) => {
+    res.send('create task')
+}
+
+const getTask = (req, res) => {
+    res.send('create task')
+}
+
+const updateTask = (req, res) => {
+    res.send('create task')
+}
+
+
+const deleteTask = (req, res) => {
+    res.send('create task')
+}
+
+
+
+module.exports ={
+    getAllTasks, getTask, updateTask, 
+}
